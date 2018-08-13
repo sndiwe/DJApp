3a9ccc3e5839f7e4ce642a45510a085b7442e779
Sam Ndiwe <mrndiwe@gmail.com>
2018-08-13 11:44:26 -0400

    Basic setup for vue.js and project homepage

---
+++ dj-vue/src/components/homePage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>What Do You Need To Do?</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'homePage',
+  data () {
+    return {
+      msg: "Welcome to DJ's App"
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
